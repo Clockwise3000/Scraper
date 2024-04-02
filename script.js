61b5c2cfb033ef8471ba7d395285cccb8371ee21
Clockwise <clockwise@myyahoo.com>
2024-04-02 10:56:43 -0700

    Better?

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
 function authenticate() {
     var password = prompt("Enter the password:");
 
-    if (password === "your_password") {
+    if (password === "qXnu[f6T7D)>&?$jaQ.+HD1]Sj<pIo=oL,@[~zBfG|`U;c'@93(j2hTZ[aXpUU-") {
         sessionStorage.setItem("authenticated", "true");
-        window.location.href = "/passwordprotected/mainpage.html";
+        window.location.href = "Scraper/3F4818AC1DC3F53F1D91A6CE1BCFE1BDDFBE28A456D7C848599A395721/mainpage.html";
     } else {
         alert("Incorrect password. Please try again.");
     }
 }
 
 window.onload = function() {
-    if (sessionStorage.getItem("authenticated") !== "true" && window.location.pathname.startsWith("/passwordprotected")) {
+    if (sessionStorage.getItem("authenticated") !== "true" && window.location.pathname.startsWith("/Scraper/3F4818AC1DC3F53F1D91A6CE1BCFE1BDDFBE28A456D7C848599A395721")) {
         alert("Unauthorized access. Please enter the correct password to access the content.");
         window.location.href = "/";
     }
